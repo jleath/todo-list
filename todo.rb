e7be74e06e2f9d275918996add31ab938d3a5d00
Joshua Leath <lawnfurniture@icloud.com>
2021-01-24 17:47:05 -0500

    code cleanup

--- todo.rb
+++ todo.rb
@@ -1,6 +1,6 @@
-require "sinatra"
-require "sinatra/reloader" if development?
-require "tilt/erubis"
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'tilt/erubis'
 
 configure do
   enable :sessions
@@ -11,32 +11,32 @@ before do
   session[:lists] ||= []
 end
 
-get "/" do
-  redirect "/lists"
+get '/' do
+  redirect '/lists'
 end
 
 # View all lists
-get "/lists" do
+get '/lists' do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
 # Create a new list
-post "/lists" do
+post '/lists' do
   list_name = params[:list_name].strip
   error = list_name_error(list_name)
   if error
     session[:error] = error
     erb :new_list, layout: :layout
   else
-    session[:lists] << {name: list_name, todos: []}
+    session[:lists] << { name: list_name, todos: [] }
     session[:success] = "The list \"#{list_name}\" has been created."
-    redirect "/lists"
+    redirect '/lists'
   end
 end
 
 # Render the new list form
-get "/lists/new" do
+get '/lists/new' do
   erb :new_list, layout: :layout
 end
 
@@ -45,9 +45,9 @@ private
 # Return an error message if list_name is invalid. Otherwise, return nil.
 def list_name_error(name)
   if !(1..100).cover?(name.size)
-    "The list name must be between 1 and 100 characters in length."
+    'The list name must be between 1 and 100 characters in length.'
   elsif session[:lists].any? { |list| list[:name] == name }
-    "The list name must be unique."
+    'The list name must be unique.'
   else
     nil
   end
