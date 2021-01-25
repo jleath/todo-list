29cfbb370d16f3c7b4231eebdcbfb0294c474c6a
Josh Leath <lawnfurniture@icloud.com>
2021-01-25 12:22:26 -0500

    added ability to view and edit lists

--- todo.rb
+++ todo.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'sinatra/content_for'
 require 'tilt/erubis'
 
 configure do
@@ -35,9 +36,39 @@ post '/lists' do
   end
 end
 
+# Change the name of an existing list
+post '/lists/:id' do
+  list_name = params[:list_name].strip
+  list_id = params[:id].to_i
+  @list = session[:lists][list_id]
+  error = list_name_error(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = "The list has been updated."
+    redirect "/lists/#{list_id}"
+  end
+end
+
 # Render the new list form
 get '/lists/new' do
   erb :new_list, layout: :layout
+end
+
+# Render the single list view
+get '/lists/:id' do
+  list_id = params[:id].to_i
+  @list = session[:lists][list_id]
+  erb :list, layout: :layout
+end
+
+# Render edit list view
+get '/lists/:id/edit' do
+  list_id = params[:id].to_i
+  @list = session[:lists][list_id]
+  erb :edit_list, layout: :layout
 end
 
 private
